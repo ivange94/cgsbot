663d31812354f873e485d3858784452d02f58149
Ivange Larry <ivangelarry@gmail.com>
2019-01-28 15:45:28 +0100

    Echo the URL verification challenge code back to Slack

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
-from flask import request
+from flask import Flask , request , make_response
+import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def check():
     if request.method == 'GET':
-        return 'Up and running...'
+        return make_response("These are not the slackbots you're looking for.", 404)
     else:
-        return request.form['challenge']
+        events_data = json.loads(request.data.decode('utf-8'))
+
+        if 'challenge' in events_data:
+            return make_response(
+                events_data.get('challenge'), 200, {'content_type': 'application/json'}
+            )
+        return make_response("Not implemented.", 404)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)

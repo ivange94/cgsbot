701dd5e56b50d6e10a97a3a4b6e99976c12b697b
Ivange Larry <ivangelarry@gmail.com>
2019-01-28 18:07:44 +0100

    Respond to mentions

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask , request , make_response
+from slackclient import SlackClient 
 import json
+import os
 
 app = Flask(__name__)
+
+slack_token = os.environ['SLACK_API_TOKEN']
+sc = SlackClient(slack_token)
 
 @app.route('/', methods=['GET', 'POST'])
 def check():
@@ -14,6 +19,17 @@ def check():
             return make_response(
                 events_data.get('challenge'), 200, {'content_type': 'application/json'}
             )
+
+        if "event" in events_data:
+            event_type = events_data["event"]["type"]
+
+            if (event_type == "app_mention"):
+                sc.api_call(
+                    "chat.postMessage",
+                    channel='C93RP3CSG',
+                    text="Hi, I'm Nancy. I will be your assistant. For now this is all I can do!"
+                )
+                return make_response("", 200)
         return make_response("Not implemented.", 404)
 
 if __name__ == '__main__':
